0feeb116cdf1de0065d3466737551335c1807813
John Ochs <john.w.ochs0@gmail.com>
2019-12-12 17:45:02 -0600

    added mailto in menu

--- assets/routing/htmlroutes.js
+++ assets/routing/htmlroutes.js
@@ -3,7 +3,6 @@ var path = require("path");
 module.exports = function (app) {
     app.get("/", function (req, res) {
         res.render("index");
-        // res.sendFile(path.join(__dirname, "../../views/layouts/main.handlebars"));
     });
 
     app.get("/music", function (req, res) {
@@ -17,11 +16,4 @@ module.exports = function (app) {
     app.get("/travelogue", function (req, res) {
         res.render("travelogue");
     });
-    // app.get("/survey", function (req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/survey.html"));
-    // });
-
-    // app.get("/answer", function (req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/answer.html"));
-    // });
 }
--- server.js
+++ server.js
@@ -2,18 +2,34 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var hndlbrs = require("express-handlebars");
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 var PORT = process.env.PORT || 8890;
 
-app.engine("handlebars", hndlbrs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// let transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     host: 'smtp.gmail.com',
+//     auth: {
+//         user: "john.w.ochs0@gmail.com",
+//         pass: ""
+//     }
+// });
+
+// let mailOptions = {
+//     from: "?",
+//     to: "john.w.ochs0@gmail.com",
+//     subject: "portfolio message",
+//     text: 'yeet yeet mcskeet'
+// };
+
+// transporter.sendMail(mailOptions, (err, data) => {
+//     err ? console.log(err) : console.log("mailed");
+// });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("app"));
-app.use(express.static(path.join(__dirname, './')));
-// app.use(express.static(path.join(__dirname, 'style/')));
-// app.use(express.static(path.join(__dirname, 'assets/')));
+app.use(express.static(path.join(__dirname, '/')));
+app.engine("handlebars", hndlbrs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 require("./assets/routing/htmlroutes.js")(app);
 
